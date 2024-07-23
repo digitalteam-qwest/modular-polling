c4c215357ae9820e23f90664728114760109bc10
Work <sajid.hussain@qwestservices.co.uk>
2024-07-23 09:49:05 +0100

    fixed shit

--- lambda_function.py
+++ lambda_function.py
@@ -3,17 +3,20 @@ from lookup import integrations
 def lambda_handler(event, context):
     environment = 'test'
     
+    #basic validations
     if "queryStringParameters" not in event:        
         return 'Invalid parameters'
         
     environment = event['queryStringParameters']['environment']
     integrationIDs = event['queryStringParameters']['integrationIDs']
 
+    #call the given integration and pass the given row as the payload
     def callIntegration(integrationID, row):
         payload = {
             'Section 1': {}
         }
 
+        #translate the row into payload
         if (row is not None):
             for token in row:
                 payload['Section 1'][token] = {
@@ -23,6 +26,7 @@ def lambda_handler(event, context):
 
         return integration.runLookup(integrationID, payload)['data']
 
+    #for each given row, call the next integration in the list
     def something(index, rows):
         if (rows is not None):
             for i in rows:
@@ -30,17 +34,19 @@ def lambda_handler(event, context):
 
                 if (len(integrationIDs) > index + 1):
                     newIndex = index + 1
+                    #
                     something(newIndex, newRows)
         else:
             newRows = callIntegration(integrationIDs[index], None)
 
             if (len(integrationIDs) > index + 1):
                 newIndex = index + 1
-                return something(newIndex, newRows)
+                something(newIndex, newRows)
     
     integration = integrations(environment)
     integration.login()
 
+    #start with the first integration in the list
     something(0, None)
 
     return 'All good'
@@ -53,5 +59,5 @@ def lambda_handler(event, context):
 #        "integrationIDs": ['669f586074915','669f58853d1f4','669f58d9c5804']
 #    }
 #}
-#
+
 #lambda_handler(event,None)
