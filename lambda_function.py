0fc720066b1ba1305c47ffde0a12372764c334e7
hussain-qwest <sajid.hussain@qwestservices.co.uk>
2024-07-25 22:44:26 +0100

    updated config, optimised code and introduced unit test

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 from lookup import integrations
 
 def lambda_handler(event, context):
+
     environment = 'test'
     
     #basic validations
@@ -26,38 +27,20 @@ def lambda_handler(event, context):
 
         return integration.runLookup(integrationID, payload)['data']
 
-    #for each given row, call the next integration in the list
-    def something(index, rows):
-        if (rows is not None):
-            for i in rows:
-                newRows = callIntegration(integrationIDs[index], rows[i])
-
-                if (len(integrationIDs) > index + 1):
-                    newIndex = index + 1
-                    #
-                    something(newIndex, newRows)
-        else:
-            newRows = callIntegration(integrationIDs[index], None)
+    #for each given row, call the given integration
+    def recursiveSomething(index = 0, rows = {'0': None}):
+        for i in rows:
+            newRows = callIntegration(integrationIDs[index], rows[i])
 
             if (len(integrationIDs) > index + 1):
                 newIndex = index + 1
-                something(newIndex, newRows)
+                recursiveSomething(newIndex, newRows)
     
+    #integrations initialisation
     integration = integrations(environment)
     integration.login()
 
-    #start with the first integration in the list
-    something(0, None)
+    #start
+    recursiveSomething()
 
     return 'All good'
-    
-
-# test code
-#event = {
-#    "queryStringParameters": {
-#        "environment": 'test',
-#        "integrationIDs": ['669f586074915','669f58853d1f4','669f58d9c5804']
-#    }
-#}
-
-#lambda_handler(event,None)
---
+++ test.py
@@ -0,0 +1,27 @@
+from lambda_function import lambda_handler
+import unittest
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_increment_pass(self):
+        event = {
+            "queryStringParameters": {
+                "environment": 'test',
+                "integrationIDs": ['669f586074915','669f58853d1f4','669f58d9c5804']
+            }
+        }
+
+        self.assertEqual(lambda_handler(event, None), 'All good')
+
+    # This test is designed to fail for demonstration purposes.
+    #def test_decrement_fail(self):
+    #    event = {
+    #        "queryStringParameters": {
+    #            "environment": 'test',
+    #            "integrationIDs": ['669f586074915','669f58853d1f4','669f58d9c5804']
+    #        }
+    #    }
+    #    
+    #    self.assertEqual(lambda_handler(event, None), 'No good')
+
+if __name__ == '__main__':
+    unittest.main()
